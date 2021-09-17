c9178a22eb098d0c76761282904833a55c1df8cb
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2021-09-17 07:16:29 +0700

    updated setup.msi url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var config = {
   appName: "VRMBA Website",
-  ue4clientPath: "https://firebasestorage.googleapis.com/v0/b/bonvr-39527.appspot.com/o/Setup%2FBonVR-UE4-Client.msi?alt=media&token=3ac14162-2396-4e8e-88a7-320733c08a4a",
+  ue4clientPath: "https://firebasestorage.googleapis.com/v0/b/bonvr-39527.appspot.com/o/Setup%2FBonVR-UE4-Client.msi?alt=media&token=8de77f1e-8aab-48d4-8a30-9c1610229073",
   apiPath: "https://us-central1-bonvr-39527.cloudfunctions.net/api/", 
   // apiPath: "http://localhost:5001/bonvr-39527/us-central1/api/",
 };
